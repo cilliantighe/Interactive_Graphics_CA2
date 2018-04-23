5df20ae5e0f5b31434951bac749d3fa8a9d056a7
N00152737 <N00152737@student.iadt.ie>
2018-04-23 16:39:26 +0100

    Commented project code

--- js/CelestialObject.js
+++ js/CelestialObject.js
@@ -1,5 +1,35 @@
-function CelestialObject(_rad, _dis, _orb, _col) {
+/*
+CelestialObject is the name given to the class for this project. The main
+reason for this name is that this class will be used to create a sun, planets
+and moons. It was originally called 'Planet' from the coding train challenge.
+The class is passed 4 variables.
 
+_rad -----> Refers to the radius size of the object
+
+_dis -----> Refers to the distance from the child CelestialObject is to it's parent.
+The distance is set to zero as the sun is drawn first and that has no parent.
+
+_orb -----> Refers to the orbital speed that the CelestialObject has to it's parent
+Again, this is set to zero as the sun is not orbiting anything.
+
+_col -----> Refers to the colour of the CelestialObject object that it is given
+*/
+function CelestialObject(_rad, _dis, _orb, _col) {
+  /*
+  this.CbSize -----> Refers to the size of the CelestialObject currently in the draw loop.
+  This increments in size until it has met the required size (this.radius * 2)
+
+  this.ObSize -----> Refers to the distance the orbital outline is currently in the loop.
+  This increments in size until it has met the required size (_dis * 2)
+
+  this.angle -----> Refers to the angle the CelestialObject body is drawn in
+  relation to it's parent. The number returned is in radians
+
+  this.children -----> Refers to an array of CelestialObject that are children
+  to it's parent. This is initialized in a function below
+
+  this.planetColours -----> Refers to an array of pre-made colours for the CelestialObject objects
+  */
   this.CbSize = 0;
   this.ObSize = 0;
   this.angle = random(TWO_PI);
@@ -10,21 +40,49 @@ function CelestialObject(_rad, _dis, _orb, _col) {
   this.planetColours = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#e74c3c']
   this.colour = _col;
 
+  /*
+  This function is used to display the objects. The 'push' and 'pop' functions
+  are used to constrain all transformations, styles etc. to each object. This ensures
+  that transformations don't stack on top of each other resulting in a chain of planets
+  orbiting one after another.
+  */
   this.render = function() {
     push();
     noStroke();
     fill(this.colour);
+    /*
+    It's important to rotate the object before you translate it as the rotation
+    will be incorrect
+    */
     rotate(this.angle);
     translate(this.distance, 0);
+    /*
+    The statement below checks whether the CelestialObject object has reached it's
+    desired size yet. It will continue to increase in size until the condition
+    is no longer met.
+    */
     if (this.CbSize >= (this.radius * 2)) {
       ellipse(0, 0, this.CbSize, this.CbSize);
     } else {
       ellipse(0, 0, this.CbSize, this.CbSize);
       this.CbSize = this.CbSize + 0.5;
     }
+    /*
+    This statement checks to see whether the parent has children and has reached
+    it's desired size. The point of this statement is to let the parent continue
+    to grow until it's finished and then it will move onto it's children and start
+    to draw them
+    */
     if (this.children != null && this.CbSize >= (this.radius * 2)) {
       for (var i = 0; i < this.children.length; i++) {
-        var check = this.children[i].outline(this.children[i].distance);
+        /*
+        The for loop goes through the parent's children and calls the orbitalOutline
+        function. It draws an ellipse until it has reached it's appropiate size
+        (the distance from the parent). The function will return a 'true' or 'false'
+        statement to tell whether the the orbitalOutline has reached it's size.
+        Once the function returns a true value, it will then render the children
+        */
+        var check = this.children[i].orbitalOutline(this.children[i].distance);
         if (check) {
           this.children[i].render();
         }
@@ -33,6 +91,11 @@ function CelestialObject(_rad, _dis, _orb, _col) {
     pop();
   }
 
+  /*
+  The function is called to move the current position of the CelestialObject obejct
+  The orbital speed is added to the current angle of the object. The function is
+  called for any children too.
+  */
   this.orbit = function() {
     this.angle = this.angle + this.orbitSpeed;
     if (this.children != null) {
@@ -42,10 +105,16 @@ function CelestialObject(_rad, _dis, _orb, _col) {
     }
   }
 
-  this.outline = function(_rad) {
+
+  /*
+  This function is called to draw the orbital outline of each child. The orbital
+  outline is increased in size until it has reached the correct size. The function
+  returns false until the size is met which it then returns a true value
+  */
+  this.orbitalOutline = function(_dis) {
     stroke(255, 50);
     noFill();
-    if (this.ObSize >= (_rad * 2)) {
+    if (this.ObSize >= (_dis * 2)) {
       ellipse(0, 0, this.ObSize, this.ObSize);
       return true;
     } else {
@@ -55,17 +124,41 @@ function CelestialObject(_rad, _dis, _orb, _col) {
     }
   }
 
+  /*
+  This function creates children objects of the parent. It also creates children
+  for children if desired. This is related to the 'level' variable. In the coding
+  train challenge, he uses the level function to help keep track of how many levels
+  of CelestialObject objects he wanted to create. In my example, I have the sun,
+  planets and moons for those planets.
+  */
   this.spawnChildren = function(_amt, _level) {
+    // Loop that executes the code for the required amount of objects to be drawn
     for (var i = 0; i < _amt; i++) {
       var rad = this.radius / (_level * random(2, 6));
+      /*
+      This is the check whether the current objects being drawn are 'moons'. If
+      level is equal to 2 then that means that they are moons and I want the distance
+      from the planet to the moons to be tight
+      */
       if (_level == 2) {
         var dis = random(this.radius + rad, (this.radius + rad) * 2);
       } else {
+        /*
+        This is a random formulae that I came up with to space out the planets
+        from the sun
+        */
         var dis = (this.radius * 2) + (rad * 2) * i;
       }
+      // Random orbital speed
       var orb = random(-0.005, 0.005);
-      var col = int(random(this.planetColours.length));
+      // Random number based off the length of the array to select a colour from it
+      var col = floor(random(this.planetColours.length));
       this.children[i] = new CelestialObject(rad, dis, orb, this.planetColours[col]);
+
+      /*
+      A statement to check whether the current level is less than two. If this
+      value is increased, then there will be mulitple layers of children with children
+      */
       if (_level < 2) {
         var total = int(random(0, 4));
         this.children[i].spawnChildren(total, _level + 1);
@@ -73,70 +166,3 @@ function CelestialObject(_rad, _dis, _orb, _col) {
     }
   }
 }
-
-// function CelestialObject(_rad, _dis, _orb, _level) {
-//
-//   this.level = _level;
-//   this.location = createVector(width / 2, height / 2);
-//   this.radius = _rad;
-//   this.distance = createVector(_dis, _dis / 2);
-//   this.children = [];
-//   this.orbitSpeed = _orb;
-//   this.t = random(360);
-//
-//   this.render = function() {
-//     noStroke();
-//     fill(255);
-//     if (this.level === 1) {
-//       ellipse(this.location.x, this.location.y, this.radius * 2, this.radius * 2);
-//     } else if (this.level === 2) {
-//       this.location.x = this.distance.x * Math.cos(this.t * Math.PI / 180) + width / 2;
-//       this.location.y = this.distance.y * Math.sin(this.t * Math.PI / 180) + height / 2;
-//       if(this.count === 0){
-//         var x = this.location.x;
-//         var y= this.location.y;
-//         this.count = 1;
-//       }
-//       ellipse(this.location.x, this.location.y, this.radius * 2, this.radius * 2);
-//       this.t = this.t + 1;
-//     }
-//     if (this.children != null) {
-//       for (var i = 0; i < this.children.length; i++) {
-//         this.children[i].render();
-//         this.children[i].outline(x, y);
-//       }
-//     }
-//   }
-//
-//   this.orbit = function() {
-//     this.angle = this.angle + this.orbitSpeed;
-//     if (this.children != null) {
-//       for (var i = 0; i < this.children.length; i++) {
-//         this.children[i].orbit();
-//       }
-//     }
-//   }
-//
-//   this.outline = function(_xHeight, _yHeight) {
-//     stroke(255, 100);
-//     noFill();
-//     ellipse(width / 2, height / 2, _xHeight, _yHeight);
-//   }
-//
-//   this.spawnChildren = function(_amt, _level) {
-//     for (var i = 0; i < _amt; i++) {
-//       var rad = this.radius / (_level * random(2, 6));
-//       if (_level == 2) {
-//         var dis = random(this.radius + rad, (this.radius + rad) * 2);
-//       } else {
-//         var dis = (this.radius * 2) + (rad * 2) * i;
-//       }
-//       var orb = random(-0.01, 0.01);
-//       this.children[i] = new CelestialObject(rad, dis, orb, 2);
-//       if (_level < 2) {
-//         var total = int(random(0, 4));
-//         this.children[i].spawnChildren(total, _level + 1);
-//       }
-//     }
-//   }
-// }
--- js/sketch.js
+++ js/sketch.js
@@ -5,10 +5,15 @@ Student No: N00152737
 Animation Sketch
 */
 
-var x = 0;
-var y = 0;
+/*
+amt -----> Refers to the amount of 'stars' to be drawn
+
+pointArray -----> Refers to the 2d array that will be used to hold (x, y)
+locations for drawing stars in the background
+
+sunRad -----> Refers to the radius of the sunRad
+*/
 var amt = 1000;
-var time = 0;
 var pointArray = [];
 var sunRad = 50;
 
@@ -16,10 +21,8 @@ var sunRad = 50;
 function setup() {
 
   // Creates the canvas for the animation to be displayed on
-  // Gets the width of the div so the canvas can take up all the space
-  var cWidth = window.outerWidth;
-  var cHeight = window.outerHeight;
-  var canvas = createCanvas(cWidth, cHeight);
+  // Gets the width of the screen so the canvas can take up all the space
+  var canvas = createCanvas(window.outerWidth, window.outerHeight);
 
   // Repositioning the canvas
   canvas.parent('container');
@@ -27,27 +30,39 @@ function setup() {
   // Setting the color of the canvas background
   background('#070000');
 
+  /*
+  Creating an array of the star points to be drawn. The (x, y) values for each
+  point needs to be initialized in the setup so that they are redrawn in the
+  same location on each drawn
+  */
   for (var i = 0; i < amt; i++) {
     pointArray[i] = [int(random(width)), int(random(height))];
   }
 
+  // Parameters are (_rad, _dis, _orb, _col)
   sun = new CelestialObject(sunRad, 0, 0, '#FFA803');
-  sun.spawnChildren(15, 1);
+  // Parameters are (_amt, _level)
+  sun.spawnChildren(10, 1);
 }
 
 // The 'draw' function is called in a loop. Everything that is in the function is executed continuously
 function draw() {
 
+  // Redraws the background
   background('#070000');
+
+  // Stroke colour of white for the stars
   stroke(255);
+
+  // The nested for loop below is for extracting the (x, y) locations from the 2d array.
   for (var i = 0; i < pointArray.length; i++) {
     for (var j = 0; j < 1; j++) {
       point(pointArray[i][j], pointArray[i][j + 1]);
     }
   }
 
+  // Translates the canvas to the centre of the screen so that everything is drawn from here.
   translate(width / 2, height / 2);
   sun.render();
   sun.orbit();
-  time++;
 }
